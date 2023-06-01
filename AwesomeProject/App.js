92a7c33f7ac3f5ee1fd09698b1510a95d42f6318
chemvic <chemvic79@gmail.com>
2023-06-01 16:11:34 +0300

    123

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Привет!!! Проверка работы эмуляции)))</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React, { useState } from "react";
+import { StyleSheet, Text, ScrollView, SafeAreaView } from "react-native";
+
+const COURSES = [
+  {
+    id: "1",
+    title: "HTML",
+  },
+  {
+    id: "2",
+    title: "JavaScript",
+  },
+  {
+    id: "3",
+    title: "React",
+  },
+  {
+    id: "4",
+    title: "React Native",
+  },
+];
 
 export default function App() {
+  const [courses, setCourses] = useState(COURSES);
+
   return (
-    <View style={styles.container}>
-      <Text>Привет!!! Проверка работы эмуляции)))</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {courses.map((course) => (
+          <Text key={course.id}>{course.title}</Text>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
   },
 });
